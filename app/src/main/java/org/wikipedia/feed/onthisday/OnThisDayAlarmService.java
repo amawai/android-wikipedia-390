1b04a6abc3c0d70ca84ed6a1e23254a44047df27
Aman Bhandal <aman_bhandal@live.ca>
2018-02-01 23:48:04 -0500

    mplemented Notification Service for On This Day Articles - Added OnThisDayAlarmService for initializing the Alarm  - Updated Manifest to include Receiver - Added OnThisDayNotifications for showing the On This Day notifications  - Updated String resource file to include onThisDay title and description

---
+++ app/src/main/java/org/wikipedia/feed/onthisday/OnThisDayAlarmService.java
@@ -0,0 +1,32 @@
+package org.wikipedia.feed.onthisday;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.icu.util.Calendar;
+
+// This class is responsible for creating the alarm service that will trigger the
+// On This Day notifications
+
+public class OnThisDayAlarmService {
+    private Context context;
+    private PendingIntent mAlarmSender;
+
+    public OnThisDayAlarmService(Context context) {
+        this.context = context;
+        mAlarmSender = PendingIntent.getBroadcast(context, 0, new Intent(context, OnThisDayNotifications.class), 0);
+    }
+
+    // Initializes the alarm
+    public void startAlarm(){
+        //Set the alarm to trigger to 2 PM everyday
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.HOUR_OF_DAY, 14);
+        long daily = c.getTimeInMillis();
+        // Schedule the alarm!
+        AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        am.setRepeating(AlarmManager.RTC, daily, AlarmManager.INTERVAL_DAY,mAlarmSender);
+    }
+
+}
---
+++ app/src/main/java/org/wikipedia/feed/onthisday/OnThisDayNotifications.java
@@ -0,0 +1,39 @@
+package org.wikipedia.feed.onthisday;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import org.wikipedia.R;
+
+// Displays the notification regarding the On This Day Articles
+public class OnThisDayNotifications extends BroadcastReceiver {
+    // Sets the ID for the notification
+    private static final String CHANNEL_ID = "ON_THIS_DAY_CHANNEL";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent onIntent = new Intent(context, OnThisDayActivity.class);
+        PendingIntent pintent = PendingIntent.getActivity(context, 0, onIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle(context.getString(R.string.notification_on_this_day_title))
+                .setContentText(context.getString(R.string.notification_on_this_day_desc))
+                .setContentIntent(pintent)
+                .setAutoCancel(true);
+
+        if (true) {
+            notificationBuilder.setSmallIcon(R.drawable.ic_w_transparent);
+        } else {
+            notificationBuilder.setSmallIcon(R.mipmap.launcher);
+        }
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, notificationBuilder.build());
+    }
+
+}
+
