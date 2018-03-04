4b07e31bc93955308116b12e7643053d6de04319
Aman Bhandal <aman_bhandal@live.ca>
2018-03-04 01:17:50 -0500

    [#22] Added tests for DestinationActivity

---
+++ app/src/test/java/org/wikipedia/travel/destinationpicker/DestinationFragmentTest.java
@@ -0,0 +1,22 @@
+package org.wikipedia.travel.destinationpicker;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by aman_ on 3/4/2018.
+ */
+public class DestinationFragmentTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onCreateView() throws Exception {
+    }
+
+    @Test
+    public void getDestinationString() throws Exception {
+    }
+
+}
