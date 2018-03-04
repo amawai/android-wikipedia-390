534745f9246f93c6e57bbc83129ba642547ec9be
Aman Bhandal <aman_bhandal@live.ca>
2018-03-04 02:43:48 -0500

    [#22] Added tests for DestinationFragment, robolectric keeps failing with ResourceNotFoundException - need help with this.

--- app/src/test/java/org/wikipedia/travel/destinationpicker/DestinationFragmentTest.java
+++ app/src/test/java/org/wikipedia/travel/destinationpicker/DestinationFragmentTest.java
@@ -1,22 +1,28 @@
 package org.wikipedia.travel.destinationpicker;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.wikipedia.BuildConfig;
+
+import static org.junit.Assert.assertNotNull;
+import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
 
 /**
  * Created by aman_ on 3/4/2018.
  */
-public class DestinationFragmentTest {
-    @Before
-    public void setUp() throws Exception {
-    }
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, packageName = "org.wikipedia", manifest = "AndroidManifest.xml")
+public class DestinationFragmentTest {
+
+    
     @Test
     public void onCreateView() throws Exception {
-    }
-
-    @Test
-    public void getDestinationString() throws Exception {
+        DestinationFragment destinationFragment = new DestinationFragment();
+        startFragment(destinationFragment);
+        assertNotNull(destinationFragment);
     }
 
 }
