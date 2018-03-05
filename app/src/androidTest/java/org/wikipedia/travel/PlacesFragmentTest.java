9a2a008205858964c5cc2b502c0d59eb683b37af
Maxim Nguyen <mnhn329@gmail.com>
2018-03-04 21:54:06 -0500

    #22 more import removal

--- app/src/androidTest/java/org/wikipedia/travel/PlacesFragmentTest.java
+++ app/src/androidTest/java/org/wikipedia/travel/PlacesFragmentTest.java
@@ -1,16 +1,9 @@
 package org.wikipedia.travel;
 
-import android.content.Context;
-import android.content.Intent;
-import android.test.MoreAsserts;
-
-import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+
 
 /**
  * Created by mnhn3 on 2018-03-04.
--- app/src/test/java/org/wikipedia/travel/PlaceFragmentTest.java
+++ app/src/test/java/org/wikipedia/travel/PlaceFragmentTest.java
@@ -4,10 +4,6 @@ import android.test.MoreAsserts;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.junit.Assert.assertThat;
-
 /**
  * Created by mnhn3 on 2018-03-04.
  */
