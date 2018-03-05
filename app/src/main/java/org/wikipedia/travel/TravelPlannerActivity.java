aa45a984a06d2b2c9c826ee504644a91f4245851
SunXP <roger_lu_dude@hotmail.com>
2018-03-04 23:19:55 -0500

    [#20] Changed back the travel fragment and removed uncessary comments in the strings.xml file

--- app/src/main/java/org/wikipedia/travel/TravelPlannerActivity.java
+++ app/src/main/java/org/wikipedia/travel/TravelPlannerActivity.java
@@ -12,13 +12,13 @@ import org.wikipedia.activity.SingleFragmentActivity;
  * Created by Artem on 2018-02-26.
  */
 
-public class TravelPlannerActivity extends SingleFragmentActivity<TravelDatePickerFragment> {
+public class TravelPlannerActivity extends SingleFragmentActivity<TravelFragment> {
     public static Intent newIntent(@NonNull Context context) {
         return new Intent(context, TravelPlannerActivity.class);
     }
 
     @Override
-    protected TravelDatePickerFragment createFragment() {
-        return new TravelDatePickerFragment();
+    protected TravelFragment createFragment() {
+        return new TravelFragment();
     }
 }
