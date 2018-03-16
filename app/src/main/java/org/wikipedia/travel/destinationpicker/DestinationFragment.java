552d4af001243a9adb435234cd8cd55c182720e0
Aman Bhandal <aman_bhandal@live.ca>
2018-03-15 21:38:54 -0400

    [#56] Added list layout for design, added left swipe for delete function to list, using a mock list for now.

--- app/src/main/java/org/wikipedia/travel/destinationpicker/DestinationFragment.java
+++ app/src/main/java/org/wikipedia/travel/destinationpicker/DestinationFragment.java
@@ -1,5 +1,6 @@
 package org.wikipedia.travel.destinationpicker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,10 +8,16 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -18,7 +25,14 @@ import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 import org.wikipedia.R;
+import org.wikipedia.concurrency.CallbackTask;
+import org.wikipedia.travel.database.TripDbHelper;
 import org.wikipedia.travel.datepicker.DateActivity;
+import org.wikipedia.travel.trip.Trip;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,10 +44,15 @@ import butterknife.Unbinder;
 
 public class DestinationFragment extends Fragment implements View.OnClickListener {
 
+    private DestinationAdapter destinationAdapter;
+
+    private List<Trip> userDestinationList = new ArrayList<>();
+
     private Unbinder unbinder;
     private static String[] destinationString;
 
     @BindView(R.id.destination_button_next) FloatingActionButton nextButton;
+    @BindView(R.id.destination_list_view) RecyclerView destinationList;
 
     //The method will assemble the destinationFragment and invoke the Google Place Autocomplete widget
     @Nullable
@@ -45,6 +64,14 @@ public class DestinationFragment extends Fragment implements View.OnClickListene
         getAppCompatActivity().getSupportActionBar().setTitle(getString(R.string.view_travel_card_title));
 
         nextButton.setOnClickListener(this);
+
+        updateUserDestinationList();
+        destinationAdapter = new DestinationAdapter(getContext());
+        destinationList.setAdapter(destinationAdapter);
+        destinationList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Adds event listener for when user swipes destination history left
+        onSwipeLeft();
 
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getActivity().getFragmentManager().findFragmentById(R.id.fragment_place_autocomplete);
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
@@ -59,6 +86,28 @@ public class DestinationFragment extends Fragment implements View.OnClickListene
             }
         });
         return view;
+    }
+
+    private void onSwipeLeft() {
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int direction) {
+                final int position = viewHolder.getAdapterPosition(); //get position which is swiped
+
+                // Removes destination from list when user swipes left
+                if (direction == ItemTouchHelper.LEFT) {
+                    userDestinationList.remove(position);
+                    destinationAdapter.notifyDataSetChanged();
+                }
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(destinationList); //set swipe to destinationList
     }
 
     public void onClick(View v) {
@@ -81,6 +130,92 @@ public class DestinationFragment extends Fragment implements View.OnClickListene
     //Returns the destinationArray, to be used for getting wikipedia articles for city surrounding
     public static String[] getDestinationString() {
         return destinationString;
+    }
+
+    private void updateUserDestinationList() {
+        CallbackTask.execute(() -> TripDbHelper.instance().getAllLists(),  new CallbackTask.DefaultCallback<List<Trip>>(){
+            @Override
+            public void success(List<Trip> list) {
+                if (getActivity() == null) {
+                    return;
+                }
+                userDestinationList = list;
+                destinationAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    //Adapter for the RecyclerView
+    public final class DestinationAdapter extends RecyclerView.Adapter<TripItemHolder> {
+        private Context context;
+
+        public DestinationAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public TripItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_trip_list, parent, false);
+            return new TripItemHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(TripItemHolder holder, int position) {
+            holder.bindItem(userDestinationList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return userDestinationList.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        // Insert a new item to the RecyclerView on a predefined position, could be used in the future
+        public void insert(int position, Trip data) {
+            userDestinationList.add(position, data);
+            notifyItemInserted(position);
+        }
+
+        // Remove a RecyclerView item containing a specified Data object, could be used in the future
+        public void remove(int data) {
+            int position = userDestinationList.indexOf(data);
+            userDestinationList.remove(position);
+            notifyItemRemoved(position);
+        }
+
+    }
+
+    //Individual rows that hold information about a trip
+    public final class TripItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public RelativeLayout tripLayout;
+        public TextView tripName;
+        public TextView tripDate;
+        private int index;
+
+        public TripItemHolder(View tripView) {
+            super(tripView);
+            tripLayout = (RelativeLayout) tripView.findViewById(R.id.trip_info);
+            tripName = (TextView) tripView.findViewById(R.id.trip_name_view_text);
+            tripDate = (TextView) tripView.findViewById(R.id.trip_date_view_text);
+            tripLayout.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position >= 0) {
+                Toast.makeText(getContext(), "You selected the trip " + userDestinationList.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        public void bindItem(Trip trip) {
+            tripName.setText(trip.getTitle());
+            tripDate.setText(trip.getTripDepartureDate().toString());
+        }
     }
 
 }
