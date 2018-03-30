81d6c87d6d58f3d2bd9331b8556023b8bab01a7e
Aman Bhandal <aman_bhandal@live.ca>
2018-03-30 17:57:55 -0400

    [#85] Implemented unit test for API execution

---
+++ app/src/test/java/org/wikipedia/translation/TranslatorTest.java
@@ -0,0 +1,37 @@
+package org.wikipedia.translation;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by aman_ on 3/30/2018.
+ */
+public class TranslatorTest{
+
+    @Test
+    public void testExecute() {
+        Translator mockTranslator = PowerMockito.mock(Translator.class);
+        try{
+            when(mockTranslator.execute("hello","en","fr").get()).thenReturn("bonjour");
+            assertEquals("bonjour", mockTranslator.execute("hello","en","fr"));
+        }catch(Exception e) {
+            System.out.print("Error" + e.getMessage());
+        }
+    }
+}
