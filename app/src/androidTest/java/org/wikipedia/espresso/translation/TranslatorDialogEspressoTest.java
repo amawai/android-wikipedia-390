13d285eb6113cfac071de37c38ea090de30bcd86
Aman Bhandal <aman_bhandal@live.ca>
2018-03-30 22:40:36 -0400

    [#84] Updated LongPressEspressoTest and added base method for TranslatorDialogEspressoTest. Moved translator button to after Copy

--- app/src/androidTest/java/org/wikipedia/espresso/translation/TranslatorDialogEspressoTest.java
+++ app/src/androidTest/java/org/wikipedia/espresso/translation/TranslatorDialogEspressoTest.java
@@ -1,45 +1,43 @@
 package org.wikipedia.espresso.translation;
 
-
 import android.graphics.Rect;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiSelector;
-import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.wikipedia.R;
 import org.wikipedia.travel.TravelPlannerActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.Matchers.allOf;
 
 /**
- * Purpose: This tests the longClick functionality within an article for the Translate button
+ * Created by amawai on 14/03/18.
  */
 
-@LargeTest
-@RunWith(AndroidJUnit4.class)
-public class LongPressEspressoTest {
-
+public class TranslatorDialogEspressoTest {
     private UiDevice mDevice;
 
     @Rule
     public ActivityTestRule<TravelPlannerActivity> dActivityTestRule = new ActivityTestRule<>(TravelPlannerActivity.class);
 
     @Before
-    public void setUp() throws UiObjectNotFoundException {
+    public void setUp() throws UiObjectNotFoundException{
         //Allows testing of components outside of testing context, in this case: destination
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
 
@@ -64,7 +62,7 @@ public class LongPressEspressoTest {
     }
 
     @Test
-    public void ensureLongPressDisplaysActionBar() throws UiObjectNotFoundException {
+    public void translatorDialogAppearanceTest() throws UiObjectNotFoundException {
         UiObject montrealArticleCard = mDevice.findObject(new UiSelector()
                 .text("Montreal Central Station"));
         try {
@@ -77,8 +75,18 @@ public class LongPressEspressoTest {
         UiObject montrealArticle = mDevice.findObject(new UiSelector()
                 .text("Railway station in Montreal, Quebec, Canada"));
 
+
+
         Rect coordinates = montrealArticle.getBounds();
         mDevice.swipe(coordinates.centerX(), coordinates.centerY(), coordinates.centerX(), coordinates.centerY(), 100);
+
+
+        ViewInteraction translateButton = onView(withText(R.id.menu_text_select_translate));
+
+//        translateButton.perform(click());
+//
+//        UiObject translationOverlay = mDevice.findObject(new UiSelector()
+//                .text("Translation"));
 
     }
 
