e5edceb74d67ae2ef3d7969dc32fcf93fb143054
Jad Malek <jadmalek9@hotmail.com>
2018-04-07 09:07:11 -0400

    [#79] Modified the token for the tool in the travis.yml file and updated build.gradle to most recent config. Also, added in a test method to test if/how static analysis is taking place

--- app/src/main/java/org/wikipedia/feed/onthisday/OnThisDayNotifications.java
+++ app/src/main/java/org/wikipedia/feed/onthisday/OnThisDayNotifications.java
@@ -38,5 +38,15 @@ public class OnThisDayNotifications extends BroadcastReceiver {
         }
     }
 
+    //Will remove this upon confirmation of static analysis reporting
+    void testMethodForStaticAnalysis(String a, String b) {
+        if (a == null) {
+            System.out.println();
+            System.out.println();
+        } else {
+        }
+        a.length();
+    }
+
 }
 
