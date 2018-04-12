c93d5c81af17a9bbfa51e221cefd2a230f8521f5
Aman Bhandal <aman_bhandal@live.ca>
2018-04-11 20:52:15 -0400

    [#56] Added edge test cases, seperated the create and delete test into their own unit.

--- app/src/androidTest/java/org/wikipedia/travel/database/DestinationDbHelperTest.java
+++ app/src/androidTest/java/org/wikipedia/travel/database/DestinationDbHelperTest.java
@@ -1,8 +1,11 @@
 package org.wikipedia.travel.database;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.wikipedia.travel.trip.Trip;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.Assert.assertNotNull;
@@ -10,34 +13,83 @@ import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class DestinationDbHelperTest {
+    private List<Trip> testList = DestinationDbHelper.getInstance().getAllLists();
+    private List<Trip> emptyList = new ArrayList <>(0);
+    private Trip testTrip = new Trip(new Trip.Destination("Laval"));
+    private Trip nullTrip = null;
+    private long predictedSize;
+    private long actualSize;
 
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    // Tests creating a populated list in database
     @Test
-    public void testCreateAndDeleteList() {
-        List<Trip> list = DestinationDbHelper.getInstance().getAllLists();
-        Trip testTrip = new Trip(new Trip.Destination("Laval"));
-        long predictedSize;
-        long newSize;
-
-        // Test for list creation
-        list.add(testTrip);
-        DestinationDbHelper.getInstance().createList(list.get(list.size() - 1).getDestination());
-        newSize = DestinationDbHelper.getInstance().getAllLists().size();
-        predictedSize = list.size();
-        assertEquals(predictedSize, newSize);
-
-        // Test for list deletion
-        list = DestinationDbHelper.getInstance().getAllLists();
-        DestinationDbHelper.getInstance().deleteList(list.get(list.size() - 1));
-        list.remove(list.size() - 1);
-        newSize = DestinationDbHelper.getInstance().getAllLists().size();
-        predictedSize = list.size();
-        assertEquals(predictedSize, newSize);
+    public void testCreateList() {
+        testList.add(testTrip);
+        DestinationDbHelper.getInstance().createList(testList.get(testList.size() - 1).getDestination());
+        actualSize = DestinationDbHelper.getInstance().getAllLists().size();
+        predictedSize = testList.size();
+        assertEquals(predictedSize, actualSize);
     }
 
+    // Tests deleting the created Trip from database
+    @Test
+    public void testDeleteList() {
+        testList = DestinationDbHelper.getInstance().getAllLists();
+        DestinationDbHelper.getInstance().deleteList(testList.get(testList.size()-1));
+        testList.remove(testList.size() - 1);
+        actualSize = DestinationDbHelper.getInstance().getAllLists().size();
+        predictedSize = testList.size();
+        assertEquals(predictedSize, actualSize);
+    }
+
+    // Tests deleting a null list in database, expecting NullPointerException
+    @Test
+    public void testDeleteNullList() {
+        exception.expect(NullPointerException.class);
+        DestinationDbHelper.getInstance().deleteList(nullTrip);
+    }
+
+    // Tests deleting an empty list in database, expecting IndexOutOfBoundsException
+    @Test
+    public void testDeleteEmptyList() {
+        exception.expect(IndexOutOfBoundsException.class);
+        DestinationDbHelper.getInstance().deleteList(emptyList.get(0));
+    }
+
+    // Tests deleting a null list in database, expecting NullPointerException
+    @Test
+    public void testCreateNullList() {
+        exception.expect(NullPointerException.class);
+        DestinationDbHelper.getInstance().createList(nullTrip.getDestination());
+    }
+
+    // Tests creating an empty list in database, expecting IndexOutOfBoundsException
+    @Test
+    public void testCreateEmptyList() {
+        exception.expect(IndexOutOfBoundsException.class);
+        DestinationDbHelper.getInstance().createList(emptyList.get(0).getDestination());
+    }
+
+    // Tests getting list from database
     @Test
     public void testGetAllLists() {
-       int size = DestinationDbHelper.getInstance().getAllLists().size();
-       assertTrue(size >= 0);
+       actualSize = DestinationDbHelper.getInstance().getAllLists().size();
+       assertTrue(actualSize >= 0);
        assertNotNull(DestinationDbHelper.getInstance().getAllLists());
+    }
+
+    // Tests deleting a list that does not exist in the database
+    @Test
+    public void testDeleteNonExistentTrip() {
+        testList = DestinationDbHelper.getInstance().getAllLists();
+        predictedSize = testList.size();
+        // Adding a trip to testList that has not been added to the database
+        testList.add(testTrip);
+        //Attempting to delete the trip from the database
+        DestinationDbHelper.getInstance().deleteList(testList.get(testList.size() - 1));
+        actualSize = DestinationDbHelper.getInstance().getAllLists().size();
+        assertEquals(predictedSize, actualSize);
     }
 }
